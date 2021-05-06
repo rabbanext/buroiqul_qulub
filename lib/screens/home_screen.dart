be4e5e5ce82d54662c2524fc21acc76c6c760a4f
rabbanos <59020772+rabbanos@users.noreply.github.com>
2021-05-06 17:47:55 +0700

    Update home_screen.dart
    update home screen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:buroiqul_qulub/screens/halaman_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,124 +8,88 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final List<String> pages = <String>['PageTawasul', 'PageDuafajr', 'PageWirdullatif', 'PageRotibulhaddad', 'PageRotibulattos', 'PageWirdusakron', 'PageWiridbadasholat', 'PageDoaTahajud', 'PageSholathajat', 'PageDoabirwalidain', 'PageYasin'];
   final List<String> judul = <String>['Tawasul', 'Dua Fajr', 'Wirdullatif', 'rotibul Haddad', 'Rotibulattos', 'Wird sakronn', 'Wirid bada sholat', 'DoaTahajud', 'Sholathajat', 'Doa Birwalidain', 'PageYasin'];
-  final List<int> colorCodes = <int>[600, 500, 600, 500, 100, 500, 600, 500, 100, 100, 300];
-
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  void _openEndDrawer() {
-    _scaffoldKey.currentState.openEndDrawer();
-  }
-  void _closeEndDrawer() {
-    Navigator.of(context).pop();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0, //main color
-        iconTheme: IconThemeData(color: Colors.black),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.menu_rounded),
-            tooltip: 'Pengaturan',
-            color: Colors.black,
-            onPressed: _openEndDrawer,
-          ),
-        ],
-      ),
       body: SafeArea(
-        child: InteractiveViewer(
-          child: Column(
-            children: [
-              // SizedBox(height: 20,),
-              Text(
-                'Burooooo',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontWeight: FontWeight.w900,
-                  fontSize: 26,
+        child: Stack(
+          children: [
+            Container(
+              color: Color(0xFFe7edf7),
+            ),
+            Container(
+              padding: EdgeInsets.all(24),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  "Buroiqul Qulub",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 29,
+                    fontWeight: FontWeight.w900
+                  ),
                 ),
               ),
-              SizedBox(height: 20,),
-              Expanded(
-                child: ListView.builder(
-                    padding: const EdgeInsets.all(12),
-                    itemCount: pages.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        padding: const EdgeInsets.only(bottom: 12),
-                        child: Material(
-                          color: Colors.green[colorCodes[index]],
-                          child: InkWell(
-                            splashColor: Theme.of(context).primaryColorLight,
-                            child: Container(
-                              height: 100,
-                              child: Center(
-                                  child: Text(
-                                      '> ${judul[index]}',
-                                      style: TextStyle(
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 12, right: 12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                      color: Color(0xFFF4F7FB),
+                    ),
+                    height: 94 * judul.length.toDouble(),
+                    child: ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        padding: const EdgeInsets.all(12),
+                        itemCount: pages.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(0xFFCDCFD0).withOpacity(0.2),
+                                  spreadRadius: 2,
+                                  blurRadius: 8,
+                                ),
+                              ],),
+                            padding: const EdgeInsets.only(bottom: 12),
+                            child: Material(
+                              borderRadius: BorderRadius.all(Radius.circular(12)),
+                              color: Colors.white,
+                              child: InkWell(
+                                borderRadius: BorderRadius.all(Radius.circular(12)),
+                                splashColor: Theme.of(context).primaryColorLight,
+                                child: Container(
+                                  height: 70,
+                                  child: Center(
+                                      child: Text(
+                                        judul[index],
+                                        style: TextStyle(
                                           fontWeight: FontWeight.w900,
                                           fontSize: 26,
-                                      ),
-                                  )
+                                        ),
+                                      )
+                                  ),
+                                ),
+                                onTap: () {Navigator.pushNamed(context, pages[index]);
+                                },
                               ),
                             ),
-                            onTap: () {Navigator.pushNamed(context, pages[index]);
-                            },
-                          ),
-                        ),
-                      );
-                    }
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      endDrawer: Drawer(
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Image.network(
-                      "https://img.icons8.com/plasticine/100/000000/name.png")),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    "asasas",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w900,
+                          );
+                        }
                     ),
-                  ),
-                ),
+                  )
+                ],
               ),
-              Container(
-                height: 40,
-                margin: EdgeInsets.only(bottom: 40),
-                child: GestureDetector(
-                  onTap: () {
-                    _closeEndDrawer();
-                  },
-                  child: Text(
-                    "   KELUAR   ",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.red,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
